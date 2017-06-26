3b526c7fdca0f27f7a259d8356871a9153695e30
smoisset <smoisset@worldremit.com>
2017-06-26 14:19:31 +0100

    Update distServer.js

---
+++ buildScripts/distServer.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from  'path';
+import open from 'open';
+import compression from 'compression';
+
+/* eslint-disable no-console */
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', (req, res) => {
+  // Hard coding for simplicity. Pretend this hits a real db
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tmorton@yahoo.com"},
+    {"id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com"}
+  ]);
+});
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
