ab540bcde1017a6567a8c11538b77a41211b1aa8
smoisset <smoisset@worldremit.com>
2017-06-26 09:37:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { getUsers } from './api/userApi';
+import './index.css';
+import {getUsers, deleteUser} from './api/userApi';
 
-// Populate table of users via API call.
 getUsers().then(result => {
   let usersBody = "";
 
   result.forEach(user => {
-    usersBody += `<tr>
+    usersBody+= `<tr>
       <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
       <td>${user.id}</td>
       <td>${user.firstName}</td>
@@ -15,4 +15,16 @@ getUsers().then(result => {
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
